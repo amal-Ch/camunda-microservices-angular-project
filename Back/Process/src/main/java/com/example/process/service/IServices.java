ce56e7775ba662a5fba0e9bb795a82b33eea14a0
amal-Ch <amal.chibani@esprit.tn>
2024-09-22 17:49:03 +0100

    commit1Back

---
+++ Back/Process/src/main/java/com/example/process/service/IServices.java
@@ -0,0 +1,25 @@
+package com.example.process.service;
+
+import com.example.process.DTO.RequestDTO;
+import com.example.process.entity.Request;
+import com.example.process.entity.WorkflowProcess;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IServices {
+    ///////////////////PROCESSES///////////////////////
+    List<WorkflowProcess> getAllProcesses();
+    Optional<WorkflowProcess> getProcessById(Integer id);
+    WorkflowProcess createProcess(WorkflowProcess process);
+    WorkflowProcess updateProcess(Integer id, WorkflowProcess processDetails);
+    void deleteProcess(Integer id);
+
+///////////////////REQUESTES///////////////////////
+     List<RequestDTO> getAllRequests();
+
+    RequestDTO addRequest(RequestDTO requestDTO);
+    Request saveRequest(Request request);
+    RequestDTO updateRequest(Integer id, RequestDTO requestDTO);
+     void deleteRequest(Integer id);
+}
